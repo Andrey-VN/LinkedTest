6cba348827237910a901dee6428b573d5fd27e0a
Вылегжанин Андрей <andre-vylegzhani@yandex.ru>
2021-02-05 16:41:01 +0300

    Реализовал классы для работы с очереддью и стеком. Использована реализация добавления и удаления на основе списков. Списки использованы односвязаные

---
+++ Queue/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+
+    public class QueueList<T>
+    {
+        class Node
+        {
+            public Node Next { get; set; }
+            public T Value { get; set; }
+            public Node(T val)
+            {
+                Value = val;
+            }
+        }
+
+        Node head;
+        Node tail;
+        int Count { get; set; }
+
+        public void Enqueue(T item)
+        {
+            Node node = new Node(item);
+            if (Count == 0)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail.Next = node;
+                tail = node;
+            }
+            Count++;
+        }
+
+        public T Dequeue()
+        {
+            if(Count!=0)
+            {
+                Node node = head;
+
+                head = head.Next;
+                Count--;
+                return node.Value;
+               
+            }
+            return default;
+        }
+        public T Peek()
+        {
+            return head.Value;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QueueList<int> queueList = new QueueList<int>();
+
+            queueList.Enqueue(1);
+            queueList.Enqueue(2);
+            queueList.Enqueue(3);
+            queueList.Enqueue(4);
+            queueList.Enqueue(5);
+
+            Console.WriteLine(queueList.Dequeue());
+            Console.WriteLine(queueList.Dequeue());
+            Console.WriteLine(queueList.Dequeue());
+            Console.WriteLine(queueList.Dequeue());
+            Console.WriteLine(queueList.Dequeue());
+
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ StackList/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackList
+{
+
+    public class StackL<T>
+    {
+        class Node
+        {
+            public Node Next { get; set; }
+            public T Value { get; set; }
+            public Node(T val)
+            {
+                Value = val;
+            }
+        }
+
+        Node head { get; set; }
+        int Count { get; set; }
+
+        public void Push(T item)
+        {
+            Node node = new Node(item);
+            if(Count == 0)
+            {
+                head = node;
+            }
+            else
+            {
+                node.Next = head;
+                head = node;
+            }
+            Count++;
+
+        }
+        public T Pop()
+        {
+            if (Count != 0)
+            {
+                Node item = head;
+                head = head.Next;
+                Count--;
+                return item.Value;
+            }
+            return default;
+            
+        }
+        public T Peek()
+        {
+            return head.Value;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<int> vs = new Stack<int>();
+            vs.Push(1);
+            vs.Push(2);
+            vs.Push(3);
+            vs.Push(4);
+            vs.Push(5);
+            Console.WriteLine(vs.Pop());
+            Console.WriteLine(vs.Pop());
+            Console.WriteLine(vs.Pop());
+            Console.WriteLine(vs.Pop());
+            Console.WriteLine(vs.Peek());
+            Console.Read();
+        }
+    }
+}
